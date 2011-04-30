cb428065469b7a260a0e3e52642de2b778b0edc5
Kai Diefenbach <kai.diefenbach@iqpp.de>
2011-04-30 08:29:23 +0200

    Get ready for release 1.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.1'
+version = '1.0.2'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
@@ -30,7 +30,7 @@ setup(name='django-workflows',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          'django-permissions >=1.0,<1.1',
+          'django-permissions == 1.0.3',
       ],
       entry_points="""
       # -*- Entry points: -*-
